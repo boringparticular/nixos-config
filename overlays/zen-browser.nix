f00af301ca0b3af38580f3d9a0a8112f2a523e9c
Kai Mies <develop@regnavi.de>
2024-11-07 14:28:17 +0100

    feat: update zen browser version

--- overlays/zen-browser.nix
+++ overlays/zen-browser.nix
@@ -2,8 +2,8 @@
   zen-browser = inputs.zen-browser.packages.${prev.system}.specific.overrideAttrs (
     oldAttrs: {
       src = builtins.fetchTarball {
-        url = "https://github.com/zen-browser/desktop/releases/download/1.0.1-a.5/zen.linux-specific.tar.bz2";
-        sha256 = "sha256:0rj8x0b7vnl6zj8mky9qip54l1cp8rpy0465gr28zsjnjhp7qyak";
+        url = "https://github.com/zen-browser/desktop/releases/download/1.0.1-a.17/zen.linux-specific.tar.bz2";
+        sha256 = "0ahpig3kafphg0pnkl1r60b9phfp5s2rilpxb20q2fwz41d1miwi";
       };
     }
   );
