debf0b41d5e84da60375c361ce11c37e74786997
Kai Mies <develop@regnavi.de>
2024-11-08 10:50:23 +0100

    fix(yazi): use stable package version

--- home/kmies/common/core/yazi.nix
+++ home/kmies/common/core/yazi.nix
@@ -1,6 +1,7 @@
-{
+{pkgs, ...}: {
   programs.yazi = {
     enable = true;
+    package = pkgs.stable.yazi;
     enableZshIntegration = true;
     enableFishIntegration = true;
     enableNushellIntegration = true;
