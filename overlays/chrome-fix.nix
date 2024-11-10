76c9551a02fcd5118cf7f1133159de9fc24c8a03
Kai Mies <develop@regnavi.de>
2024-11-10 09:13:17 +0100

    fix: allow insecure electron version

--- overlays/chrome-fix.nix
+++ overlays/chrome-fix.nix
@@ -1,7 +1,12 @@
 {inputs, ...}: final: prev: let
   pkgs = import inputs.nixpkgs-working-chrome-electron {
     inherit (final) system;
-    config.allowUnfree = true;
+    config = {
+      allowUnfree = true;
+      permittedInsecurePackages = [
+        "electron-27.3.11"
+      ];
+    };
   };
 in {
   vscode = pkgs.vscode;
