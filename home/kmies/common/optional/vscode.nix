e440858d0642c25555c5389c046f872a7d7cca46
Kai Mies <develop@regnavi.de>
2024-11-10 09:20:37 +0100

    fix: improve vscode wayland integration

--- home/kmies/common/optional/vscode.nix
+++ home/kmies/common/optional/vscode.nix
@@ -3,7 +3,8 @@
     enable = true;
     package = pkgs.vscode.override {
       commandLineArgs = [
-        "--enable-features=WaylandWindowDecorations"
+        /*
+           "--enable-features=WaylandWindowDecorations"
         "--ozone-platform-hint="
         # "--ozone-platform=wayland"
         # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
@@ -13,7 +14,20 @@
         "--enable-wayland-ime"
 
         # enable hardware acceleration - vulkan api
-        "--enable-features=Vulkan"
+        # "--enable-features=Vulkan"
+        */
+
+        "--ozone-platform-hint=auto"
+        "--ozone-platform=wayland"
+        # make it use GTK_IM_MODULE if it runs with Gtk4, so fcitx5 can work with it.
+        # (only supported by chromium/chrome at this time, not electron)
+        "--gtk-version=4"
+        # make it use text-input-v1, which works for kwin 5.27 and weston
+        "--enable-wayland-ime"
+
+        # TODO: fix https://github.com/microsoft/vscode/issues/187436
+        # still not works...
+        "--password-store=gnome" # use gnome-keyring as password store
       ];
     };
   };
