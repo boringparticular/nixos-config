b91a1a3c88790bdebb3a6c4446d330fa4b3096e6
Kai Mies <develop@regnavi.de>
2024-11-28 19:02:02 +0100

    feat(zeus): add multiple kernel specialisations
    the amdgpu drivers are broken since linux 6.10
    now i have the option to use the lts version 6.6 or xanmod or the latest
    kernel with a patch for the amdgpu drivers

--- hosts/common/optional/desktop.nix
+++ hosts/common/optional/desktop.nix
@@ -5,8 +5,60 @@
 }: {
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  specialisation."linux-xanmod" = {
+    inheritParentConfig = true;
+    configuration = {
+      boot.loader.grub.configurationName = "linux-xanmod-stable";
+      boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_xanmod_stable;
+    };
+  };
+
+  specialisation."linux-latest-patched" = {
+    inheritParentConfig = true;
+    configuration = {
+      boot.loader.grub.configurationName = "linux-latest-patched";
+      boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+      boot.kernelPatches = [
+        {
+          name = "drm-amdgpu-fix";
+          patch = builtins.fetchurl {
+            url = "https://gitlab.freedesktop.org/-/project/4522/uploads/ce146c4a85e9b9f7e4b3c81f3b3be492/v2-0001-drm-amdgpu-Fix-UVD-contiguous-CS-mapping-problem.patch";
+            sha256 = "1b6pkji0q0ybk4xy9h3z5lvh74vp5la0x78cisqbn9gicd8w69lj";
+          };
+        }
+      ];
+    };
+  };
+  specialisation."linux-xanmod-patched" = {
+    inheritParentConfig = true;
+    configuration = {
+      boot.loader.grub.configurationName = "linux-xanmod-patched";
+      boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_xanmod_stable;
+      boot.kernelPatches = [
+        {
+          name = "drm-amdgpu-fix";
+          patch = builtins.fetchurl {
+            url = "https://gitlab.freedesktop.org/-/project/4522/uploads/ce146c4a85e9b9f7e4b3c81f3b3be492/v2-0001-drm-amdgpu-Fix-UVD-contiguous-CS-mapping-problem.patch";
+            sha256 = "1b6pkji0q0ybk4xy9h3z5lvh74vp5la0x78cisqbn9gicd8w69lj";
+          };
+        }
+      ];
+    };
+  };
+
+  # The amdpgu drivers work with this kernel
+  specialisation."linux-6.6" = {
+    inheritParentConfig = true;
+    configuration = {
+      boot.loader.grub.configurationName = "linux-6.6";
+      boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_6_6;
+    };
+  };
+
   boot.plymouth.enable = true;
   boot.initrd.systemd.enable = true;
+
+  boot.kernel.sysctl."kernel.sysrq" = 1;
 
   time.timeZone = "Europe/Berlin";
 
